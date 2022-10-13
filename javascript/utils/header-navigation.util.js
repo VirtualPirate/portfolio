5b6506085f2233f834fcc4238792f856229f9ca1
VirtualPirate <artaza13020artaza@gmail.com>
2022-10-14 00:16:22 +0530

    Added left swipe close side nav event

--- javascript/utils/header-navigation.util.js
+++ javascript/utils/header-navigation.util.js
@@ -14,3 +14,9 @@ const closeSideNavBtn = document.querySelector(".close-side-nav-btn");
 closeSideNavBtn.addEventListener("click", () => {
   toggleNavSideBar();
 });
+
+let container = document.querySelector(".side-nav");
+let listener = SwipeListener(container);
+container.addEventListener("swipe", function (e) {
+  if (e.detail.directions.left) toggleNavSideBar();
+});
